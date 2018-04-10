75616cf83b00d977acef4294226d204db46d7d2c
Bwambocos <mijyumaru2525@outlook.jp>
2018-04-10 15:57:11 +0900

    Game: フィールド画像 追加
    ・ゲームフィールドの背景画像追加

--- Gear Attack/Game.cpp
+++ Gear Attack/Game.cpp
@@ -2,11 +2,13 @@
 #include <Siv3D.hpp>
 #include <HamFramework.hpp>
 #include "Game.h"
+#include "Select.h"
 
 // ゲーム画面 初期化
 Game::Game(const InitData& init) :IScene(init)
 {
-
+	gameField = Texture(U"data/Game/gameField.png");
+	fieldData = Grid<int>(fieldSize / cellSize, fieldSize / cellSize);
 }
 
 // ゲーム画面 更新
@@ -18,5 +20,5 @@ void Game::update()
 // ゲーム画面 描画
 void Game::draw() const
 {
-
+	gameField.draw(0, 0);
 }
--- Gear Attack/Game.h
+++ Gear Attack/Game.h
@@ -4,10 +4,17 @@
 #include <HamFramework.hpp>
 #include "Main.h"
 
+// const
+const int fieldSize = 392;
+const int cellSize = 28;
+
 // ƒQ[ƒ€‰æ–Ê
 class Game :public SceneManager<String>::Scene
 {
 private:
+	Texture gameField;
+	Grid<int>fieldData;
+	int stageNum, diffNum;
 
 public:
 	Game(const InitData& init);
