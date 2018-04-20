66f47ed967095370254430077fa64e8c4e12b258
Bwambocos <mijyumaru2525@outlook.jp>
2018-04-20 16:01:44 +0900

    Game: プレーヤー処理 追加
    ・プレーヤーの更新・描画を追加

--- Gear Attack/Game.cpp
+++ Gear Attack/Game.cpp
@@ -21,18 +21,89 @@ Game::Game(const InitData& init) :IScene(init)
 		}
 		fieldReader.readChar();
 	}
+	playerImg = Texture(U"data/Game/playerImg.png");
 	infoRect = Rect(490, 10, 220, 107);
 	timeRect = Rect(490, 127, 220, 107);
 	lifeRect = Rect(490, 244, 220, 96);
 	statsFont = Font(36, Typeface::Bold);
-	nowTime = startTime = Time::GetMillisec();
+	mainTime.nowTime = mainTime.startTime = Time::GetMillisec();
 	playerHP = maxHP;
+	do
+	{
+		playerX = Random(fieldSize / cellSize - 1);
+		playerY = Random(fieldSize / cellSize - 1);
+	} while (fieldData[(int)playerX][(int)playerY] != 0);
+	playerMoveFlag = false;
+	playerXMoveDistance = playerYMoveDistance = 0;
 }
 
 // ƒQ[ƒ€‰æ–Ê XV
 void Game::update()
 {
-	nowTime = Time::GetMillisec();
+	mainTime.nowTime = Time::GetMillisec();
+	if (playerMoveFlag == -1)
+	{
+		if (playerY > 0 && (KeyW.pressed() || KeyUp.pressed()))
+		{
+			if (fieldData[(int)playerX][(int)playerY - 1] != 1) playerMoveFlag = 1;
+		}
+		if (playerY < fieldSize / cellSize - 1 && (KeyS.pressed() || KeyDown.pressed()))
+		{
+			if (fieldData[(int)playerX][(int)playerY + 1] != 1) playerMoveFlag = 2;
+		}
+		if (playerX > 0 && (KeyA.pressed() || KeyLeft.pressed()))
+		{
+			if (fieldData[(int)playerX - 1][(int)playerY] != 1) playerMoveFlag = 3;
+		}
+		if (playerX < fieldSize / cellSize - 1 && (KeyD.pressed() || KeyRight.pressed()))
+		{
+			if (fieldData[(int)playerX + 1][(int)playerY] != 1) playerMoveFlag = 4;
+		}
+		if (playerMoveFlag != -1) playerTime.nowTime = playerTime.startTime = Time::GetMillisec();
+	}
+	else
+	{
+		playerTime.nowTime = Time::GetMillisec();
+		if (playerTime.nowTime - playerTime.startTime > playerMoveMilliSec)
+		{
+			switch (playerMoveFlag)
+			{
+			case 1:
+				--playerY;
+				break;
+			case 2:
+				++playerY;
+				break;
+			case 3:
+				--playerX;
+				break;
+			case 4:
+				++playerX;
+				break;
+			}
+			playerMoveFlag = -1;
+			playerXMoveDistance = playerYMoveDistance = 0;
+		}
+		else
+		{
+			auto dis = (double)cellSize*(playerTime.nowTime - playerTime.startTime) / playerMoveMilliSec;
+			switch (playerMoveFlag)
+			{
+			case 1:
+				playerYMoveDistance = -dis;
+				break;
+			case 2:
+				playerYMoveDistance = dis;
+				break;
+			case 3:
+				playerXMoveDistance = -dis;
+				break;
+			case 4:
+				playerXMoveDistance = dis;
+				break;
+			}
+		}
+	}
 }
 
 // ƒQ[ƒ€‰æ–Ê •`‰æ
@@ -46,6 +117,7 @@ void Game::draw() const
 			fieldCellImg[fieldData[x][y]].draw(44 + x * cellSize, 44 + y * cellSize);
 		}
 	}
+	playerImg.draw(44 + cellSize * playerX + playerXMoveDistance, 44 + cellSize * playerY + playerYMoveDistance);
 	infoRect.draw(Color(255, 255, 255, 120));
 	infoRect.drawFrame(2);
 	timeRect.draw(Color(64, 64, 64, 120));
@@ -69,7 +141,7 @@ void Game::draw() const
 		break;
 	}
 	statsFont(U"Œo‰ßŽžŠÔ").draw(495, 132, Color(200, 200, 200));
-	statsFont(Format((nowTime - startTime) / 1000.)).draw(495, 183, Color(200, 200, 200));
+	statsFont(Format((mainTime.nowTime - mainTime.startTime) / 1000.)).draw(495, 183, Color(200, 200, 200));
 	statsFont(U"Žc‚è‘Ì—Í").draw(495, 249, Palette::Hotpink);
 	RoundRect(495, 300, 210, 35, 2).draw(Palette::Black);
 	RoundRect(495, 300, 210 * playerHP / maxHP, 35, 2).draw((playerHP >= 100 ? Palette::Lightgreen : (playerHP >= 50 ? Palette::Yellow : Palette::Red)));
--- Gear Attack/Game.h
+++ Gear Attack/Game.h
@@ -9,18 +9,26 @@ const int fieldSize = 392;
 const int cellSize = 28;
 const int maxHP = 200;
 const int fieldCellKinds = 3;
+const double playerMoveMilliSec = 100;
+
+// ç\ë¢ëÃ
+struct doubleTime
+{
+	uint64 nowTime, startTime;
+};
 
 // ÉQÅ[ÉÄâÊñ 
 class Game :public MyApp::Scene
 {
 private:
-	Texture gameFieldImg, fieldCellImg[fieldCellKinds];
+	Texture gameFieldImg, fieldCellImg[fieldCellKinds], playerImg;
 	Grid<int>fieldData;
 	TextReader fieldReader;
 	Rect infoRect, lifeRect, timeRect;
 	Font statsFont;
-	uint64 nowTime, startTime;
-	int playerHP;
+	doubleTime mainTime, playerTime;
+	int playerHP, playerMoveFlag;
+	double playerX, playerY, playerXMoveDistance, playerYMoveDistance;
 
 public:
 	Game(const InitData& init);
